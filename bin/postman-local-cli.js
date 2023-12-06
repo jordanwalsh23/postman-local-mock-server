6c54f981dea1d0908d4d389835ac9b86803d01ec
Johannes Nicolai <johannes.nicolai@postman.com>
2023-12-06 13:01:10 -0500

    Support http(s) URLs to point to collections
    * -c parameter now supports URLs starting with http
    * moved code into main method to be able to use await for downloading collections
    
    Co-authored-by: Mark Christian Reid <mark-christian.reid@postman.com>
    Co-authored-by: Johannes Nicolai <johannes.nicolai@postman.com>
    Co-authored-by: Vitor Monteiro <vitor.monteiro@postman.com>

--- bin/postman-local-cli.js
+++ bin/postman-local-cli.js
@@ -4,46 +4,55 @@ const fs = require('fs');
 const PostmanLocalMockServer = require('../lib/server');
 
 const options = yargs
- .usage("Usage: -c <filename>")
+ .usage("Usage: -c <filename or URL>")
  .option("c", { alias: "collection", describe: "Path to your collection file.", type: "string", demandOption: true })
  .option("p", { alias: "port", describe: "The port you would like to use.", type: "string", default: 3555, demandOption: false })
  .option("v", { alias: "verbose", describe: "Verbose output", type: "boolean", default: false, demandOption: false })
  .argv;
 
-if(options.collection) {
+async function main() {
 
-  let collection, port, verbose;
-  try {
-    //Create the collection object.
-    collection = JSON.parse(fs.readFileSync(options.collection, 'utf8'));
+  if(options.collection) {
 
-    if(options.port && !isNaN(options.port)) {
-      port = options.port;
-    } else {
-      //This will never work so will always throw an error.
-      port = 99999;
+    let collection, port, verbose;
+    try {
+      // if collection is a url starting with http, try to download it
+      if (options.collection.startsWith('http')) {
+        collection = await fetch(options.collection).then(response => response.json());
+      } else {
+        collection = JSON.parse(fs.readFileSync(options.collection, 'utf8'));
+      }
+
+      if(options.port && !isNaN(options.port)) {
+        port = options.port;
+      } else {
+        //This will never work so will always throw an error.
+        port = 99999;
+      }
+
+      if(options.verbose) {
+        verbose = options.verbose
+      }
+
+      //Create a new server
+      let server = new PostmanLocalMockServer(port, collection, verbose);
+
+      //Start the server
+      server.start();
+
+    } catch(e) {
+      if(e.code == 'ENOENT') {
+        console.log(`Error starting local mock server. Collection file not found: "${options.collection}"`)
+      } else if (e.code == 'ERR_SOCKET_BAD_PORT') {
+        console.log("Error starting local mock server. Could not bind server to port:",options.port)
+      } else {
+        console.log(e)
+      }
     }
 
-    if(options.verbose) {
-      verbose = options.verbose
-    }
-
-    //Create a new server
-    let server = new PostmanLocalMockServer(port, collection, verbose);
-
-    //Start the server
-    server.start();
-
-  } catch(e) {
-    if(e.code == 'ENOENT') {
-      console.log(`Error starting local mock server. Collection file not found: "${options.collection}"`)
-    } else if (e.code == 'ERR_SOCKET_BAD_PORT') {
-      console.log("Error starting local mock server. Could not bind server to port:",options.port)
-    } else {
-      console.log(e)
-    }
+    
+    
   }
-
-  
-  
 }
+
+main();
