bd00524fbc0ed5771f1c0857e1bf1cf0ebc80794
Xyedo <hafidmahdi23@gmail.com>
2024-07-19 15:12:41 +0700

    chore(enhancement): query param value matches on decoded value set

--- test/requestTypesTest.js
+++ test/requestTypesTest.js
@@ -22,6 +22,58 @@ describe('Different Request Types', () => {
     server.start()
   })
 
+  it('Default GET response with query param with 1 ids', async () => {
+    return await axios.get(`http://localhost:${PORT}/users?page=1&limit=1&filters=company_id+eq+2&filters=id+in+(05ee1e2d-1a80-4384-9d65-3beb16bcfd1a)`).then(res => {
+      assert(res.status, 200)
+      assert.deepEqual(res.data, {
+        metadata: {
+          count: 1,
+          page: 1,
+          total_count: 1,
+          total_page: 1
+        },
+        data: {
+          ids: ["05ee1e2d-1a80-4384-9d65-3beb16bcfd1a"],
+        }
+      })
+    })
+  })
+
+  it('Default GET response with query param with ids 9c366ded-4c52-43ed-ab73-db7566f28132 and 8810022b-1fe9-40fb-bd5b-cfb48beaa621', async () => {
+    return await axios.get(`http://localhost:${PORT}/users?page=1&limit=2&filters=company_id+eq+2&filters=id+in+(9c366ded-4c52-43ed-ab73-db7566f28132+8810022b-1fe9-40fb-bd5b-cfb48beaa621)`).then(res => {
+      assert(res.status, 200)
+      assert.deepEqual(res.data, {
+        metadata: {
+          count: 2,
+          page: 1,
+          total_count: 2,
+          total_page: 1
+        },
+        data: {
+          ids: ["9c366ded-4c52-43ed-ab73-db7566f28132", "8810022b-1fe9-40fb-bd5b-cfb48beaa621"],
+        }
+      })
+    })
+  })
+
+  it('Default GET response with query param with ids 05ee1e2d-1a80-4384-9d65-3beb16bcfd1a and 8810022b-1fe9-40fb-bd5b-cfb48beaa621', async () => {
+    return await axios.get(`http://localhost:${PORT}/users?page=1&limit=2&filters=company_id+eq+2&filters=id+in+(05ee1e2d-1a80-4384-9d65-3beb16bcfd1a+8810022b-1fe9-40fb-bd5b-cfb48beaa621)`).then(res => {
+      assert(res.status, 200)
+      assert.deepEqual(res.data, {
+        metadata: {
+          count: 2,
+          page: 1,
+          total_count: 2,
+          total_page: 1
+        },
+        data: {
+          ids: ["05ee1e2d-1a80-4384-9d65-3beb16bcfd1a", "8810022b-1fe9-40fb-bd5b-cfb48beaa621"],
+        }
+      })
+    })
+  })
+
+
   it('Success GET response test.', async () => {
     return await axios.get(`http://localhost:${PORT}/get`).then(res => {
       assert(res.data.url === 'https://postman-echo.com/get')
